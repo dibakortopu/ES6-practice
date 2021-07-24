73b8228484a79958128b706aebf568c55ec4006a
Topu <dibakortopu1@gmail.com>
2021-07-24 13:42:15 +0600

    add code

---
+++ letconst.js
@@ -0,0 +1,20 @@
+/* const hubby = "Omar";
+//hubby = "BMW"
+console.log(hubby);
+
+const numbers = [12, 42];
+//numbers[1] = 88;
+numbers.push(12);
+//numbers.pop(42);
+const nayok = {name:"sakib" , phone: 12345};
+console.log(nayok); */
+
+/* let patientName = "Abdul";
+patientName = "Fatema";
+patientName = "Khaleda";
+console.log(patientName); */
+let sum = 0;
+for(let i = 0; i < 20; i++){
+    sum = sum + i;
+}
+console.log(i);
